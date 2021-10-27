fb07108099854853683414026062b7fe002a2841
Madhu Babu Mandava <31966229+MadhuBabuMandava@users.noreply.github.com>
2021-10-27 10:53:12 +0530

    Update stats.c
    Solved the challenge

--- stats.c
+++ stats.c
@@ -1,10 +1,29 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
+    int sum = 0;
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    
+    for(int i=0; i<setlength;i++)
+    {
+      // Compute sum
+      sum+=numberset[i];
+      
+      //Find min
+      if(numberset[i]<s.min){
+      s.min = numberset[i];
+      }
+        
+      //find max
+      if(numberset[i]>s.min){
+      s.max = numberset[i];
+      }
+    }
+    s.average = sum/setlength;
+    
 }
 
 int emailAlertCallCount = 0;
